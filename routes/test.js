192ef755bb45b208be305a1968911e584e213da5
SennaLyn <totatolin@foxmail.com>
2018-03-23 14:03:54 +0800

    feat:添加倒入sql的脚本

--- routes/test.js
+++ routes/test.js
@@ -2,6 +2,7 @@ import express from 'express';
 import fs from 'fs';
 const router = express.Router();
 var db = require('../config/mysqlDB.js');
+import list from '../models/list.js';
 
 // app.all('*', function (req, res, next) {
 //  res.header("Access-Control-Allow-Origin", "*");
@@ -18,25 +19,36 @@ var db = require('../config/mysqlDB.js');
 
 
 router.get('/test', function(req, res, next) {
- //  let aaa = "CREATE TABLE account(" +
- //        "id INT NOT NULL AUTO_INCREMENT," +
- //        "email varchar(100) unique," +
- //        "username varchar(64) unique," +
- //        "password varchar(32)," +
- //        "primary key (id)" +
- //        ")";
- //  // res.header("X-Powered-By",' 3.2.1');
-	// db.query(aaa,function(err,rows){
- //    if(err){
- //      res.send({status: 'failed'});
- //    }else {
- //      res.send({status: 'success'});
- //    }
- //  });
-  var content = fs.readFileSync('./1.png', 'binary');
-  res.writeHead(200, {'Content-Type':'image/png'})
-  res.write(content, 'binary');
-  res.send();
+  let aaa = "CREATE TABLE list(" +
+        "name varchar(100) ," +
+        "description varchar(64)" +
+        ")";
+  // res.header("X-Powered-By",' 3.2.1');
+	db.query(aaa,function(err,rows){
+    if(err){
+      res.send({status: 'failed'});
+    }else {
+      list.forEach((item) => {
+        let bbb = "INSERT INTO list" +
+        "(name, description)" +
+        "value" +
+        "('" + item.name + "', '" + item.description + "');"
+        db.query(bbb,function(err,rows){
+          console.log(err)
+          if (err) {
+            // res.send(err);
+          } else {
+            // res.send({status: 'success'});
+          }
+        });
+      })
+      res.send({status: 'success'});
+    }
+  });
+  // var content = fs.readFileSync('./1.png', 'binary');
+  // res.writeHead(200, {'Content-Type':'image/png'})
+  // res.write(content, 'binary');
+  // res.send();
 });
 
 export default router
