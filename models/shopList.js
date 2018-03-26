1878a89ec76fdf1abc812798059dcf5359d69b6e
SennaLyn <totatolin@foxmail.com>
2018-03-26 19:25:26 +0800

    feat:商店列表接口

--- models/shopList.js
+++ models/shopList.js
@@ -1,15 +1,36 @@
 let shopList = [
   {
-    name: 'aaa',
-    description: 'fdafdsafdsafadsfdg'
+    shop_name: 'aaa',
+    shop_avatar: '',
+    consumeption_level: 1,
+    shop_address: 'aaaaaaaaa',
+    shop_tel: 11111111111,
+    description: 'fdafdsafdsafadsfdg',
+    shop_location: 111,
+    label: 'fdaf',
+    address_link: 'fdafdsafasd'
   },
   {
-    name: 'bbb',
-    description: 'fdgfgasfd'
+    shop_name: 'bbb',
+    shop_avatar: '',
+    consumeption_level: 2,
+    shop_address: 'bbbbbbbbb',
+    shop_tel: 22222222222,
+    description: 'fdgfgasfd',
+    shop_location: 222,
+    label: 'ffdagfdaga',
+    address_link: 'fdafdsafasd'
   },
   {
-    name: 'ccc',
-    description: 'f111111fadsfdg'
+    shop_name: 'ccc',
+    shop_avatar: '',
+    consumeption_level: 3,
+    shop_address: 'ccccccccc',
+    shop_tel: 33333333333,
+    description: 'f111111fadsfdg',
+    shop_location: 333,
+    label: 'fdafaaaaaaa',
+    address_link: 'fdafdsafasd'
   }
 ]
 
