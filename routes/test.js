ec990e3ff1a1126a217190184b9648d7ff27301c
SennaLyn <totatolin@foxmail.com>
2018-03-27 20:37:12 +0800

    feat:配置redis

--- routes/test.js
+++ routes/test.js
@@ -3,9 +3,25 @@ import fs from 'fs';
 const router = express.Router();
 var db = require('../config/mysqlDB.js');
 import list from '../models/shopList.js';
+import redis from 'redis';
+var client = redis.createClient('6379', '127.0.0.1');
+// redis 链接错误
+client.on("error", function(error) {
+     console.log(error);
+});
 
 router.get('/test', function(req, res, next) {
-  let aaa = Object.keys(req.query)[0];
+  var qe = {a: 2, b:3, c:4};
+  client.hmset('field003', qe, function(err, response) {
+      console.log("err:", err);
+      console.log("response:", response);
+      client.hmget('field003', ['a', 'c'], function (err, res) {
+          console.log(err);
+          console.log(res);
+          client.end();
+      });
+  });
+  // let aaa = Object.keys(req.query)[0];
  //  let aaa = "CREATE TABLE list(" +
  //        "name varchar(100) ," +
  //        "description varchar(64)" +
@@ -32,10 +48,11 @@ router.get('/test', function(req, res, next) {
  //      res.send({status: 'success'});
  //    }
  //  });
-  var content = fs.readFileSync('./' + aaa, 'binary');
-  res.writeHead(200, {'Content-Type':'image/png'})
-  res.write(content, 'binary');
-  res.send();
+  // var content = fs.readFileSync('./' + aaa, 'binary');
+  // res.writeHead(200, {'Content-Type':'image/png'})
+  // res.write(content, 'binary');
+  // res.send();
+
 });
 
 export default router
